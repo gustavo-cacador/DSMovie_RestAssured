aac8c33464a2a2c4f430db473cb2bfd0feb8f560
Gustavo <gustavoirl456@outlook.com>
2025-10-26 20:05:12 -0300

    configurando swagger com spring security (liberando endpoints privados)

--- src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsmovie.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +38,7 @@ public class ScoreController {
 					@ApiResponse(description = "Unprocessable Entity", responseCode = "422")
 			}
 	)
+	@SecurityRequirement(name = "bearerAuth")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PutMapping(produces = "application/json")
 	public ResponseEntity<MovieDTO> saveScore(@Valid @RequestBody ScoreDTO dto) {
